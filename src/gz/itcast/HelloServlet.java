670cad03e3cb3bf462b9c0558f3de0b3f7279ea7
luckydogjack <1435359352@qq.com>
2018-04-21 21:03:23 +0800

    submit a project day08_web.

---
+++ src/gz/itcast/HelloServlet.java
@@ -0,0 +1,20 @@
+package gz.itcast;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		//向浏览器输出内容
+		response.getWriter().write("这是第一个动态网站的动态资源："+new Date());
+	}
+
+}
